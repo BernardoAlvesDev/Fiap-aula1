343582368116b9111d8f2f129faffe24aa7fdedb
Bernardo Nascimento <bernardoalves1404@gmail.com>
2021-10-06 20:51:11 -0300

    calculadora

--- src/application/Calculator.java
+++ src/application/Calculator.java
@@ -14,23 +14,23 @@ public class Calculator {
 		int multiplicacao;
 		double divisao;
 		
-		System.out.println("Por favor sdigite o Primeiro valor:");
+		System.out.print("Por favor digite o Primeiro valor:");
 		n1 = sc.nextInt();
-		System.out.println("Por favor sdigite o Primeiro valor:");
+		System.out.print("Por favor digite o Primeiro valor:");
 		n2 = sc.nextInt();
 		
 		soma = n1 + n2;
 		subtracao = n1 - n2;
 		multiplicacao = n1 * n2;
-		divisao = (double) n1 / (double )n2;
+		divisao = (double) n1 / (double) n2;
 		
 		System.out.println("A soma é:" + soma);
 		System.out.println("A diferença é:"+ subtracao);
 		System.out.println("A multiplicacao é:" + multiplicacao);
 		System.out.printf("A divisao é: %.2f", divisao);
+		System.out.println("FIM");
 		
-		
-
+		sc.close();
 	}
 
 }
